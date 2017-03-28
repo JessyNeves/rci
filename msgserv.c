d50a9748796fc21f45872346fc4468e3b0c3eb13
JessyNeves <neves.jessy@gmail.com>
2017-03-28 13:16:15 +0100

    Best Version
    msgserv.c
    UDP connection with ID SV and rmb.c client app.
    It is receiving and storing the messages sent by the client
    
    rmb.c
    recognizes the commands and sends the messages to msgserv. Pratically
    finished

--- msgserv.c
+++ msgserv.c
@@ -12,6 +12,12 @@
 struct hostent *gethostbyname(const char *name);
 extern int errno;
 
+struct messages{
+	char message[128];
+	int timer;
+};
+
+
 // argv[1] = name of the server
 // argv[2] = IP do servidor de mensagens
 // argv[3] = Porto UDP Servidor Mensagens Atende
@@ -23,22 +29,26 @@ extern int errno;
 
 
 int main(int argc, char *argv[]){
-	int fd,fd2,fd3, n, addrlen;
-	struct sockaddr_in addr;
-	struct sockaddr_in addr_tcp;
 
-	struct hostent *h; //variavel do tipo hostent
-	struct hostent *t; // hostent for TCP
-	struct in_addr *b; //in_addr for TCP
-	struct in_addr *a; //variavel do tipo in_addr
+	struct messages array[128];
+	int fd,fd2,fd3, fd4, n, addrlen;
+	struct sockaddr_in addr;
+	struct sockaddr_in addr2;
+	struct sockaddr_in addr_tcp;
+	struct sockaddr_in addr_tcp2;
+
+	struct hostent *h; //UDP CLIENT
+	struct hostent *s; //UDP SERVER
+	struct in_addr *a; //in_addr for UDP
 	int maxsize = 128;
 	int recfrom=0, ret, maxfd;
-	int i=1;
+	int i=1, r=0, j=0;
 	char hostname[30];
 	char buffer[maxsize];
+	char info[maxsize];
 	char refresh[maxsize];
 
-	fd_set rfds, fds;
+	fd_set rfds;
 	struct timeval time;
 	time.tv_sec = 15;
 	time.tv_usec = 0;
@@ -51,56 +61,34 @@ int main(int argc, char *argv[]){
 	//se argc < 4 nao foram introduzidos argumentos obrigatorios, kill
 	if(argc < 4) exit(0);
 	if(argc != 8){
-		printf("Too few arguments. Assuming values\n");
-		if((h=gethostbyname("tejo.tecnico.ulisboa.pt"))==NULL){
-			printf("\nCan't gethostbyname!\n");
-			exit(1);//error
-		}
-/////////SOCKET UDP //////////////////////
+		if((h=gethostbyname("tejo.tecnico.ulisboa.pt"))==NULL)exit(1);//error
+
+		/////////SOCKETS UDP/TCP //////////////////////
 		a = (struct in_addr*)h->h_addr_list[0];
-		fd=socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP); //UDP
-		fd2=socket(AF_INET,SOCK_STREAM,0);
-		fd3=socket(AF_INET,SOCK_STREAM,0);	//TCP
-//TCP
-		if(fd==-1)exit(1);//error
-		if(fd2==-1)exit(1);//error
+		fd=socket(AF_INET,SOCK_DGRAM,0);//UDP to Máquina TEJO
+		fd2=socket(AF_INET,SOCK_DGRAM,0);//UDP socket for SERVER
+		fd3=socket(AF_INET,SOCK_STREAM,0);//TCP
+		fd4=socket(AF_INET,SOCK_STREAM,0); //TCP
 
-///////////////////		UDP		///////////////////////////////////
-	memset((void*)&addr_tcp,(int)'\0',sizeof(addr_tcp)); //clears address
+///////////////////		UDP Structures	/////////////////
 	memset((void*)&addr,(int)'\0',sizeof(addr)); //clears the vector
+	memset((void*)&addr2,(int)'\0',sizeof(addr2)); //clears the vector
+
 	addr.sin_family=AF_INET; //protocol UDP
-	addr.sin_addr= *a; //protocol UDP IP of the destiny
-	addr.sin_port=htons(59000); //protocol UDP port
-/////////////////////////////////////////////////////
+	addr.sin_addr= *a; // IP maquina TEJO
+	addr.sin_port=htons(59000); //protocol UDP port of Server ID
 
+	//SERVER UDP, accepting datagrams from any address on the system through PORT defined on program call
+	addr2.sin_family=AF_INET; //protocol UDP
+	addr2.sin_addr.s_addr=htonl(INADDR_ANY);
+	addr2.sin_port=htons(atoi(argv[3])); //protocol UDP port of Server ID
+	ret=bind(fd2,(struct sockaddr*)&addr2,sizeof(addr2));
+	if(ret==-1){
+		printf("error: %s\n",strerror(errno));
+		exit(1);//error
+	}
 
-	//TCP family specification
-		addr_tcp.sin_family=AF_INET;
-		addr_tcp.sin_addr.s_addr=htonl(INADDR_ANY);
-		addr_tcp.sin_port=htons(5000);
-		if(bind(fd2,(struct sockaddr*)&addr_tcp,sizeof(addr_tcp))==-1)exit(0);
-		if(listen(fd2,5)==-1){
-			printf("error Listen\n");
-			exit(1);//error
-		}
-
-		//Gathering information from other TCP msgserv running
-				t = gethostbyname("tejo.tecnico.ulisboa.pt");
-				b = (struct in_addr*)t->h_addr_list[0];
-				printf("internet address: %s (%08lX)\n",inet_ntoa(*b),(long unsigned int)ntohl(b->s_addr));
-		//Gathering information from other TCP msgserv running
-
-				//Connecting to TCP Server
-						n=connect(fd3,(struct sockaddr*)&addr_tcp,sizeof(addr_tcp));
-						if(n==-1){
-							printf("Connection Error - Unable to Connect n=-1\n");
-							exit(1);
-						}else{
-							printf("TCP Connection Established\n");
-							printf("----------------------------\n");
-						}
-
-			//registering the message server upon start of the program ////////////
+//registering the message server upon start of the program ////////////
 			strcat(buffer, "REG ");
 			while(i<5){
 				strcat(buffer, argv[i]);
@@ -110,14 +98,10 @@ int main(int argc, char *argv[]){
 			}
 			n=sendto(fd,buffer,maxsize,0,(struct sockaddr*)&addr,sizeof(addr));//send function
 			i=1;
-			if(n==-1){
-					printf("Couldn't send\n");
-					exit(1);
-					}
-			// No recfrom function because ID server does not respond in this case
-			///////////////////////////////////////////////////////////////
+			if(n==-1)exit(1);
+	 ///////////////NO RECVFROM BECAUSE IDENTITY WONT RESPOND////////////////////////////
 
-			////////////////////Request for other message servers connected//////////////////
+    ////////////////REQUEST OF OTHER SERVERS CONNECTED TO ID SV/////////////////////////
 			strcpy(buffer, "GET_SERVERS\n");
 			n=sendto(fd,buffer,maxsize,0,(struct sockaddr*)&addr,sizeof(addr));//send function
 			if(n==-1)exit(1);
@@ -125,27 +109,34 @@ int main(int argc, char *argv[]){
 			n=recvfrom(fd,buffer,maxsize,0,(struct sockaddr*)&addr,&addrlen);
 			if(n==-1) exit(1);
 			printf("%s", buffer);
-
-
-			/// Tratamento de dados dos outros servidores ligados
-
+			/// STORAGE OF OTHER SERVER'S INFORMATION
 			char * servers = buffer;
 			int n_servers = 0;
 			const char s[2] = ";";
 			char *token;
-			char *info;
+			char *ip;
+			char *tcport;
+			char *udport;
 			int x=0;
+
 			while(servers){
 			      char * nextLine = strchr(servers, '\n');
 			      if (nextLine) *nextLine = '\0';  // temporarily terminate the current line
-			      //printf("servers=[%s]\n", servers);
 			      token = strtok(servers, s);
 			      while( token != NULL ){
 				      x++;
-			    	  info = token;
-			    	  if(x==2)printf("%s\n", info );//IP
-			    	  if(x==3)printf("%s\n",info);//Porta UDP
-			    	  if(x==4)printf("%s\n",info);//Porta TCP
+			    	  if(x==2){
+				    	  ip = token;
+			    		  printf("%s\n", ip );//IP
+			    	  }
+			    	  if(x==3){
+				    	  udport = token;
+			    		  printf("Servidor: %s .... Porta UDP:%s\n", ip,udport);//Porta UDP
+			    	  }
+			    	  if(x==4){
+				    	  tcport = token;
+			    		  printf("Servidor: %s .... Porta TCP:%s\n", ip,tcport);//Porta TCP
+			    	  }
 			          token = strtok(NULL, s);
 			        }
 			      if (nextLine) *nextLine = '\n';  // then restore newline-char, just to be tidy
@@ -153,69 +144,100 @@ int main(int argc, char *argv[]){
 			      x=0;
 			   }
 
-			////////////////////////////////////////////////////////////////////////////////
-
-
 
 			while(1){
+				int msgnum=0;
+				char info[140];
+				char command[30];
+				char message[140];
 
 				start:
 				memset(buffer,'\0',maxsize); //clears the buffer
-				FD_ZERO(&rfds);
-				FD_SET(fd, &rfds);
-				FD_SET(0, &fds);
-				maxfd = fd+1;
+				memset(info,'\0',maxsize); //clears the buffer
+				memset(message,'\0',maxsize); //clears the buffer
 
-				ret = select(1, &fds, NULL, NULL, &time);
+				FD_ZERO(&rfds);
+				FD_SET(0, &rfds);
+				FD_SET(fd, &rfds);
+				FD_SET(fd2, &rfds);
+				FD_SET(fd3, &rfds);
+				maxfd = fd3+1;
+
+				ret = select(maxfd, &rfds, NULL, NULL, &time);
 				if(ret==0){
 					////REFRESHING SERVER////
-								strcat(buffer, "REG ");
-								while(i<5){
-									strcat(buffer, argv[i]);
-									if(i==4) break;
-									strcat(buffer, ";");
-									i++;
-								}
-								n=sendto(fd,buffer,maxsize,0,(struct sockaddr*)&addr,sizeof(addr));//send function
-								i=1;
-								if(n==-1){
-										printf("Couldn't send\n");
-										exit(1);
-										}
+					strcat(buffer, "REG ");
+					while(i<5){
+						strcat(buffer, argv[i]);
+						if(i==4) break;
+						strcat(buffer, ";");
+						i++;
+					}
+				n=sendto(fd,buffer,maxsize,0,(struct sockaddr*)&addr,sizeof(addr));//send function
+				i=1;
+				if(n==-1){
+					printf("Couldn't send\n");
+					exit(1);
+						 }
 					/////////////////////////
 					goto start;
 				}
-				if(ret) gets(buffer);
-				recfrom=strlen(buffer);
-
-				//Se recfrom>= 17 caracteres quer dizer que é o comando REG ou um comando errado que será prontamente rejeitado pelo serv indentidades
-				if(recfrom>=17)
-				{
-					n=sendto(fd,buffer,maxsize,0,(struct sockaddr*)&addr,sizeof(addr));//send function
-					if(n==-1){
-						printf("Couldn't send\n");
-						exit(1);
+				if(ret){
+					n=recvfrom(fd2,buffer,maxsize,0,(struct sockaddr*)&addr2,&addrlen);
+					if(n==-1) exit(1);
+					sscanf(buffer, "%[^ ] %[^\n]",command, info);
+					if(strcmp(command, "show_latest_messages")==0){
+						msgnum = atoi(info);
+						if(msgnum>r) msgnum = r;
+						printf("\nLatest %d messages published:\n", msgnum);
+						for(int aux=0; aux<msgnum; aux++){
+							if(array[aux].message != NULL) printf("%s\n", array[aux].message);
+						}
 					}
+
+
+					if(strcmp(command, "publish")==0){
+						if(r>0){
+							strcpy(array[r].message,info);
+							array[r].timer=array[r-1].timer + 1;
+							r++;
+							}
+						if(r==0){
+							strcpy(array[0].message,info);
+							array[0].timer = 1;
+							r++;
+							}
+					}
+
 				}
-				else
-				{
-					//outros comandos quaisquer
-
-						n=sendto(fd,buffer,maxsize,0,(struct sockaddr*)&addr,sizeof(addr));//send function
-						if(n==-1)exit(1);
-						addrlen=sizeof(addr);
-
-						n=recvfrom(fd,buffer,maxsize,0,(struct sockaddr*)&addr,&addrlen);
-						if(n==-1) exit(1);
-						n=n-2;
-						write(1,"Echo: ",6);//stdout
-						write(1,buffer,n);
-						write(1,"\n",2);
-				}
-
+				recfrom=strlen(buffer);
+//
+//				//Se recfrom>= 17 caracteres quer dizer que é o comando REG ou um comando errado que será prontamente rejeitado pelo serv indentidades
+//				if(recfrom>=17)
+//				{
+//					n=sendto(fd,buffer,maxsize,0,(struct sockaddr*)&addr,sizeof(addr));//send function
+//					if(n==-1){
+//						printf("Couldn't send\n");
+//						exit(1);
+//					}
+//				}
+//				else{
+//					//outros comandos quaisquer
+//						n=sendto(fd,buffer,maxsize,0,(struct sockaddr*)&addr,sizeof(addr));//send function
+//						if(n==-1)exit(1);
+//						addrlen=sizeof(addr);
+//
+//						n=recvfrom(fd,buffer,maxsize,0,(struct sockaddr*)&addr,&addrlen);
+//						if(n==-1) exit(1);
+//						n=n-2;
+//						write(1,"Echo: ",6);//stdout
+//						write(1,buffer,n);
+//						write(1,"\n",2);
+//				}
 			}
 
 			close(fd);
+			close(fd2);
 			exit(0);
 	}
 
--- rmb.c
+++ rmb.c
@@ -13,96 +13,72 @@
 struct hostent *gethostbyname(const char *name);
 extern int errno;
 int main(int argc, char *argv[]){
-	int fd, n, addrlen;
+	int fd, fd2, n, addrlen;
 	struct sockaddr_in addr;
-	struct hostent *h; //variavel do tipo hostent
-	struct in_addr *a; //variavel do tipo in_addr
+	struct sockaddr_in addr2;
+	struct hostent *h, *s; //variavel do tipo hostent
+	struct in_addr *a, *b; //variavel do tipo in_addr
 	int maxsize = 128;
 	char hostname[30];
 	char buffer[maxsize];
 
 
-
-
 if(argc<2){
 	printf("\n WARNING! No Arguments \n");
 		if((h=gethostbyname("tejo.tecnico.ulisboa.pt"))==NULL) exit(1);//error
+		if((s=gethostbyname("localhost"))==NULL) exit(1); //error
 
-		printf("official host name: %s\n", h->h_name);
+		printf("official host name: %s\n", h->h_name); //prints ID server name
+		printf("official host name: %s\n", s->h_name); //prints Msgserv server name
+
 		a = (struct in_addr*)h->h_addr_list[0];
+		b = (struct in_addr*)s->h_addr_list[0];
 		//printf("internet address: %s (%08lX)\n",inet_ntoa(*a),(long unsigned int)ntohl(a->s_addr));
 
-		// UDP socket ; SENDTO
-
+		// UDP sockets CREATION
+		fd2=socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP);  //UDP socket number stored in file descriptor
+			if(fd2==-1){
+				printf("ERROR\n");
+				exit(1);//error
+					}
 		fd=socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP);  //UDP socket number stored in file descriptor
 			if(fd==-1){
 				printf("ERROR\n");
 				exit(1);//error
 			}
+
+			memset((void*)&addr2,(int)'\0',sizeof(addr2)); //clears the vector
+			addr2.sin_family=AF_INET; //UDP
+			//addr2.sin_addr = *b; //IP
+			addr2.sin_port=htons(5001); //PORT
+
 			memset((void*)&addr,(int)'\0',sizeof(addr)); //clears the vector
 			addr.sin_family=AF_INET; //protocol UDP
 			addr.sin_addr= *a; //protocol UDP IP of the destiny
 			addr.sin_port=htons(59000); //predefined UDP port
 
 		while(1){
-			char tmp[maxsize];
-			memset(buffer,'\0',maxsize); //clears the buffer
-			memset(tmp,'\0',maxsize); //clears the buffer
-
 
 			int recfrom=0;
-			char message[140];
-		    fgets(buffer, maxsize , stdin);
-			recfrom=strlen(buffer);
+			fgets(buffer, maxsize , stdin);
+		    if(strcmp(buffer, "exit\n")==0)exit(0);
 
-			if(strcmp(buffer, "show_servers\n")==0) strcpy(buffer, "GET_SERVERS");
-			if(strcmp(buffer, "exit\n")==0)exit(0);
+		    if(strcmp(buffer, "show_servers\n")==0){
+		    	strcpy(buffer, "GET_SERVERS");
+		    	recfrom=strlen(buffer);
+		    	sendto(fd,buffer,maxsize,0,(struct sockaddr*)&addr,sizeof(addr));//send function
+		    	n=recvfrom(fd,buffer,maxsize,0,(struct sockaddr*)&addr,&addrlen);
+		    	if(n==-1)exit(1);
+		    	write(1,"Echo: ",6);//stdout
+		    	write(1,buffer,n);
+		    	write(1,"\n",1);
+		    }else{
+					n=sendto(fd2,buffer,maxsize,0,(struct sockaddr*)&addr2,sizeof(addr2));//send function
+					if(n==1)exit(0);
+		    	}
 
-			strncpy(tmp, buffer, 21);
-			n=strcmp(tmp, "show_latest_messages ");
-			if(n==0){
-				printf("Número de Mensagens a Apresentar: %c%c\n", buffer[21], buffer[22]);
-
-			}
-
-
-
-			sendto(fd,buffer,maxsize,0,(struct sockaddr*)&addr,sizeof(addr));//send function
-			n=recvfrom(fd,buffer,maxsize,0,(struct sockaddr*)&addr,&addrlen);
-			if(n==-1) exit(1);
-			//n=n-2;
-
-			write(1,"Echo: ",6);//stdout
-			write(1,buffer,n);
-			write(1,"\n",2);
-
-			//Se tiver 17 ou + caracteres = REG function
-
-			if(recfrom>=17)
-			{
-			n=sendto(fd,buffer,maxsize,0,(struct sockaddr*)&addr,sizeof(addr));//send function
-			if(n==-1)exit(1);
-			}
-//			else
-//			{
-//				//outros comandos quaisquer
-//
-//					n=sendto(fd,buffer,maxsize,0,(struct sockaddr*)&addr,sizeof(addr));//send function
-//					if(n==-1)exit(1);
-//					addrlen=sizeof(addr);
-//
-//					n=recvfrom(fd,buffer,maxsize,0,(struct sockaddr*)&addr,&addrlen);
-//					if(n==-1) exit(1);
-//					n=n-2;
-//
-//					write(1,"Echo: ",6);//stdout
-//					write(1,buffer,n);
-//					write(1,"\n",2);
-//			}
-		close(fd);
-		exit(0);
-				}
-			}
+		}//while
 	}
+}//main
 
 
